34c91c52427452cf2dcd50e2d545a7c3f85d6775
Marcin <ozi@post.pl>
2022-05-19 14:56:29 +0200

    Background Slider

--- 17/script.js
+++ 17/script.js
@@ -2,7 +2,7 @@
 
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=' + API_KEY + '&page=1';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_KEY + '&query=1"';
-const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w780';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
@@ -14,7 +14,7 @@ getMovies(API_URL);
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data.results);
+  // console.log(data.results);
   showMovies(data.results);
 }
 
@@ -25,7 +25,7 @@ function showMovies(movies) {
     const movieEl = document.createElement('div');
     movieEl.classList.add('movie');
     movieEl.innerHTML = `
-      <img src="${IMG_PATH + poster_path}" alt="${title}" />
+      <img src="${poster_path ? IMG_PATH + poster_path : 'no-image.png'}" alt="${title}" />
       <div class="movie-info">
         <h3>${title}</h3>
         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
